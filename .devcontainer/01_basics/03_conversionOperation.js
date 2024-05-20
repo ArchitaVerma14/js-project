dbfb4a8418dbdbd223f7cce78a8f6ea78b3dee93
ArchitaVerma14 <154065843+ArchitaVerma14@users.noreply.github.com>
2024-05-20 14:55:03 +0000

    Conversions and Operations

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,63 @@
+/*let score="sddg"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber); 
+
+// "33" => 33
+// "33sgg" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn= "sdggs"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+//"sgsd" => true
+
+let someNum=4335
+
+let stringNum=String(someNum)
+console.log(stringNum);
+console.log(typeof (stringNum));
+*/
+//*******************OPERATION ***************/
+
+let value=34
+let negValue=-value
+//console.log(negValue);
+
+/*console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
+console.log(2**3);
+console.log(2%3);*/
+
+/*let str1="hello broo"
+let str2=" how you doingngng"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("12" + 4);
+console.log(12 + "4");
+console.log("12" + 4 + 4);
+console.log(12 + 4 + "4");
+
+console.log(3+4*3);*/
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3 
+num1=num2=num3= 4+5
+
+let gameCounter = 232
+++gameCounter;
+console.log(gameCounter);
