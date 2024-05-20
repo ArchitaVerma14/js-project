2a3588dad017824e0c1d97adb60eb593127d5e9e
ArchitaVerma14 <154065843+ArchitaVerma14@users.noreply.github.com>
2024-05-20 13:56:27 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+
+    3)  //code readability should be high
+
+console.log("archita")
+
+let name="archi"
+let age=22
+let isLoggedIn=false
+let state= null
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not defined till now
+//symbol => to define uniqueness
+
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
